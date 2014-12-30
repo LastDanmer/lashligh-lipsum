9cffd958c71f9c9d790581876fc55ee7a9848fd7
Marc Bachmann <marc.brookman@gmail.com>
2014-12-30 21:45:49 +0100

    Add li alias & simplify commands

--- plugin.py
+++ plugin.py
@@ -1,25 +1,29 @@
 import pypsum
+import re
+
+types = {
+    'w' : 'words',
+    'p' : 'paragraphs',
+    'l' : 'lists',
+    'b' : 'bytes'
+}
 
 def results(fields, original_query):
     how_what = fields['~how_what']
-    hw = how_what.split(' ')
-    if len(hw) < 2:
-        return {
-                "title": "Lorem Ipsum %s ..." % hw[0]
-                }
-    how = hw[0]
-    what = hw[1]
-    lipsum = pypsum.get_lipsum(how, what, "no")
+    hw = re.match('([0-9]*)\ ?([wplb])?', how_what)
+
+    count = hw.group(1) or 1
+    type = types[hw.group(2) or 'p']
+
+    lipsum = pypsum.get_lipsum(count, type, 'no')
     output = lipsum[0].replace('\n', '<br /><br />')
     return {
-        "title": "Lorem Ipsum %s %s" % (how, what),
-        "run_args": [output] ,
-        "html": output + "<br /><br /><i>" + lipsum[1] + "</i>",
-        "webview_transparent_background": True,
+        'title': 'Lorem Ipsum %s %s' % (count, type),
+        'run_args': [output] ,
+        'html': output + '<br /><br /><i>' + lipsum[1] + '</i>',
+        'webview_transparent_background': True,
     }
 
 def run(output):
     import os
-    #os.system('echo "'+lipsum[0]+'" | pbcopy')
     os.system('echo "'+output+'" | pbcopy')
-
